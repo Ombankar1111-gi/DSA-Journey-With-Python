81614505625733b93cfdbe628dcadc2d3b5a6b5d
om-bankar <ombankar1111@gmail.com>
2026-08-12 21:12:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.47%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSameTree(self, p, q):
+        # Donhi empty astil
+        if p is None and q is None:
+            return True
+
+        # Ek empty ani ek non-empty
+        if p is None or q is None:
+            return False
+
+        # Values different
+        if p.val != q.val:
+            return False
+
+        # Left ani Right subtree check
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
